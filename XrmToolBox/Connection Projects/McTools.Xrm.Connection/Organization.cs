6a78e13aef5ec11a7e6e1086866c8c7e006b14d4
tanguy92_cp <SND\tanguy92_cp>
2014-02-19 10:57:25 +0000

    Fix projects mapping in TFS

---
+++ XrmToolBox/Connection Projects/McTools.Xrm.Connection/Organization.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xrm.Sdk.Discovery;
+
+namespace McTools.Xrm.Connection
+{
+    public class Organization
+    {
+        public OrganizationDetail OrganizationDetail { get; set; }
+
+        public override string ToString()
+        {
+            return this.OrganizationDetail.FriendlyName;
+        }
+    }
+}
