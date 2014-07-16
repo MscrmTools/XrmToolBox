9ad8e6b358fd70983833e7338711fdc50f40b839
Tanguy T <MscrmTools@users.noreply.github.com>
2014-07-16 13:49:42 +0000

    User Roles Manager : New tool

---
+++ XrmToolBox/Plugins/MsCrmTools.UserRolesManager/AppCode/RoleAction.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xrm.Sdk;
+
+namespace MsCrmTools.UserRolesManager.AppCode
+{
+    public class RoleAction
+    {
+        public List<Entity> Roles { get; set; }
+        public List<Entity> Principals { get; set; }
+
+        public int ActionType { get; set; }
+    }
+}
