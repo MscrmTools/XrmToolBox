09ef7bfb92864adb24e3a633a06637347be29062
tanguy92_cp <SND\tanguy92_cp>
2014-07-16 14:05:47 +0000

    Multiple tools : Add/Update versioning

--- XrmToolBox/Plugins/MsCrmTools.Translator/Properties/AssemblyInfo.cs
+++ XrmToolBox/Plugins/MsCrmTools.Translator/Properties/AssemblyInfo.cs
@@ -17,5 +17,5 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 [assembly: Guid("a8234074-9ff2-4a4c-b580-a2ad4507a116")]
 
-[assembly: AssemblyVersion("1.2014.5.28")]
-[assembly: AssemblyFileVersion("1.2014.5.28")]
+[assembly: AssemblyVersion("1.2014.7.16")]
+[assembly: AssemblyFileVersion("1.2014.7.16")]
