3b5250d5ee815d74acdcfb485a7b2c66699fda12
Tanguy T <MscrmTools@users.noreply.github.com>
2014-09-03 10:11:07 +0000

    FormAttributeManager : New tool

---
+++ XrmToolBox/Plugins/MsCrmTools.FormAttributeManager/AppCode/AttributeInfo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xrm.Sdk.Metadata;
+
+namespace MsCrmTools.FormAttributeManager.AppCode
+{
+    public class AttributeMetadataInfo
+    {
+        public AttributeMetadata Amd { get; set; }
+
+        public override string ToString()
+        {
+            return string.Format("{0} ({1})", Amd.LogicalName, Amd.DisplayName.UserLocalizedLabel.Label);
+        }
+    }
+}
---
+++ XrmToolBox/Plugins/MsCrmTools.FormAttributeManager/Forms/ReferenceAttributeSelector.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using Microsoft.Xrm.Client.Metadata;
+using Microsoft.Xrm.Sdk.Metadata;
+using MsCrmTools.FormAttributeManager.AppCode;
+
+namespace MsCrmTools.FormAttributeManager.Forms
+{
+    public partial class ReferenceAttributeSelector : Form
+    {
+        
+        public ReferenceAttributeSelector(IEnumerable<AttributeMetadata> amds)
+        {
+            InitializeComponent();
+
+            foreach (var amd in amds)
+            {
+                cbbAttributes.Items.Add(new AttributeMetadataInfo { Amd = amd });
+            }
+
+            cbbAttributes.SelectedIndex = 0;
+        }
+
+        public AttributeMetadata SelectedAttribute
+        {
+            get { return ((AttributeMetadataInfo) cbbAttributes.SelectedItem).Amd; }
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            DialogResult = DialogResult.Cancel;
+            Close();
+        }
+
+        private void btnOk_Click(object sender, EventArgs e)
+        {
+            DialogResult = DialogResult.OK;
+            Close();
+        }
+    }
+}
