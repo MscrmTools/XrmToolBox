92ffc0449c82e36255bcc86226e508d2b36e8a7f
Tanguy T <MscrmTools@users.noreply.github.com>
2015-01-17 11:09:29 +0000

    XrmToolBox : Updated about form to add credits for icons and tools

--- XrmToolBox/XrmToolBox/Forms/AboutForm.cs
+++ XrmToolBox/XrmToolBox/Forms/AboutForm.cs
@@ -4,6 +4,7 @@
 // BLOG: http://mscrmtools.blogspot.com
 
 using System;
+using System.Diagnostics;
 using System.Windows.Forms;
 
 namespace XrmToolBox.Forms
@@ -18,6 +19,21 @@ namespace XrmToolBox.Forms
         private void LinkCloseClick(object sender, EventArgs e)
         {
             Close();
+        }
+
+        private void llFatCow_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            Process.Start("http://www.fatcow.com/free-icons");
+        }
+
+        private void llIconsMind_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            Process.Start("https://www.iconfinder.com/iconsmind");
+        }
+
+        private void llVictorPetrovich_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            Process.Start("https://www.iconfinder.com/krasnoyarsk");
         }
     }
 }
