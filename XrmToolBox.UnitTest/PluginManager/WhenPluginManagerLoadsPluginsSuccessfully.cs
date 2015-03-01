338febe5ebfbe31132bde77acf514ccdf7ec4751
Davesmall28 <david.small@dsmall.co.uk>
2015-03-01 14:50:27 +0000

    Initial Plugin Manager unit tests.

---
+++ XrmToolBox.UnitTest/PluginManager/WhenPluginManagerLoadsPluginsSuccessfully.cs
@@ -0,0 +1,36 @@
+﻿namespace XrmToolBox.UnitTest
+{
+    using DSmall.UnitTest.Core;
+    using NUnit.Framework;
+
+    /// <summary>The when plugin manager loads plugins successfully.</summary>
+    [TestFixture]
+    public class WhenPluginManagerLoadsPluginsSuccessfully : SpecificationBase
+    {
+        private PluginManagerSpecificationFixture testFixture;
+
+        /// <summary>The should load dummy plugin correctly.</summary>
+        [Test]
+        public void ShouldLoadDummyPluginCorrectly()
+        {
+            Assert.IsTrue(testFixture.UnderTest.Plugins.Contains(typeof(DummyXrmToolBoxPlugin)));
+        }
+
+        /// <summary>The because of.</summary>
+        protected override void BecauseOf()
+        {
+            base.BecauseOf();
+
+            testFixture.UnderTest.LoadPlugins();
+        }
+
+        /// <summary>The context.</summary>
+        protected override void Context()
+        {
+            base.Context();
+
+            testFixture = new PluginManagerSpecificationFixture();
+            testFixture.PerformTestSetup();
+        }
+    }
+}
---
+++ XrmToolBox.UnitTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,21 @@
+﻿// PROJECT : XrmToolBox
+// This project was developed by Tanguy Touzard
+// CODEPLEX: http://xrmtoolbox.codeplex.com
+// GITHUB: https://github.com/MscrmTools/XrmToolBox
+// BLOG: http://mscrmtools.blogspot.com
+
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("XrmToolBox.UnitTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("MscrmTools")]
+[assembly: AssemblyProduct("XrmToolBox.UnitTest")]
+[assembly: AssemblyCopyright("Copyright © MscrmTools 2012")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("2926a57e-550d-4c94-8b2a-3e8663989a9a")]
+[assembly: AssemblyVersion("1.2015.3.1")]
+[assembly: AssemblyFileVersion("1.2015.3.1")]
