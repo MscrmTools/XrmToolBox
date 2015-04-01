4ae6b471c2db690011b19a2e5d69db37929286bc
Alexey Shytikov <alexey.shytikov@cinteros.se>
2015-04-01 23:39:53 +0200

    Declaring simple methods of interaction #85

--- XrmToolBox/IMessageBus.cs
+++ XrmToolBox/IMessageBus.cs
@@ -1,12 +1,11 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace XrmToolBox
 {
     public interface IMessageBus
     {
+        event EventHandler OnOutgoingMessage;
+
+        void OnIncomingMessage();
     }
 }
