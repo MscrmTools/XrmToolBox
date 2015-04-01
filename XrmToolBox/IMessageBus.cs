833edefa392425760f56aa9218f46ee86d2f3b27
Alexey Shytikov <alexey.shytikov@cinteros.se>
2015-04-01 23:50:02 +0200

    Introducing MessageBusArguments #85

--- XrmToolBox/IMessageBus.cs
+++ XrmToolBox/IMessageBus.cs
@@ -2,10 +2,20 @@
 
 namespace XrmToolBox
 {
+    /// <summary>
+    /// Interface which plugins should implement to be able communicate between each other
+    /// </summary>
     public interface IMessageBus
     {
-        event EventHandler OnOutgoingMessage;
+        /// <summary>
+        /// Event raised by plugin notifying broker that new message is ready to dispatch
+        /// </summary>
+        event EventHandler<MessageBusEventArgs> OnOutgoingMessage;
 
-        void OnIncomingMessage();
+        /// <summary>
+        /// Method that accepts dispatched message
+        /// </summary>
+        /// <param name="message"></param>
+        void OnIncomingMessage(MessageBusEventArgs message);
     }
 }
---
+++ XrmToolBox/MessageBusEventArgs.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace XrmToolBox
+{
+    /// <summary>
+    /// Object that passed as argument in message bus communication
+    /// </summary>
+    public class MessageBusEventArgs : EventArgs
+    {
+        public string TargetConnection;
+        
+        public string TargetPlugin;
+    }
+}
