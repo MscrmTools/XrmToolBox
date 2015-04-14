0b78ef484adbf8e192353ac8c18445b8e2f8fdac
Jonas Rapp <jonas.rapp@cinteros.se>
2015-04-14 11:14:30 +0200

    Error handling in MessageBroker if target plugin does not exist

--- XrmToolBox/MainForm.cs
+++ XrmToolBox/MainForm.cs
@@ -488,6 +488,10 @@ namespace XrmToolBox
             {
                 // Searching for suitable plugin
                 var targetModel = pManager.PluginsControls.FirstOrDefault(x => ((Type)x.Tag).GetTitle() == message.TargetPlugin);
+                if (targetModel == null)
+                {
+                    throw new PluginNotFoundException("Plugin {0} was not found", message.TargetPlugin);
+                }
                 // Displaying plugin and keeping number of the tab where it was opened
                 var tabIndex = DisplayPluginControl((UserControl)targetModel);
                 // Getting the tab where plugin was opened
