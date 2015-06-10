13e69d967159bf9e4960e43e3156763db1e0fd88
MscrmTools <tanguy92@hotmail.com>
2015-06-10 22:11:41 +0200

    Add missing files for source control

---
+++ XrmToolBox.Extensibility/Extensions.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Reflection;
+using System.Windows.Forms;
+using XrmToolBox.Extensibility.Interfaces;
+
+namespace XrmToolBox.Extensibility
+{
+    public static class Extensions
+    {
+        public static IMsCrmToolsPluginUserControl GetPlugin(this TabPage page)
+        {
+            return (IMsCrmToolsPluginUserControl)page.Controls[0];
+        }
+
+        public static ICodePlexPlugin GetCodePlexPlugin(this TabPage page)
+        {
+            // ReSharper disable once SuspiciousTypeConversion.Global
+            return page.Controls[0] as ICodePlexPlugin;
+        }
+
+        public static IGitHubPlugin GetGithubPlugin(this TabPage page)
+        {
+            // ReSharper disable once SuspiciousTypeConversion.Global
+            return page.Controls[0] as IGitHubPlugin;
+        }
+
+        public static IHelpedPlugin GetHelpEnabledPlugin(this TabPage page)
+        {
+            // ReSharper disable once SuspiciousTypeConversion.Global
+            return page.Controls[0] as IHelpedPlugin;
+        }
+
+        public static IPayPalPlugin GetPaypalPlugin(this TabPage page)
+        {
+            // ReSharper disable once SuspiciousTypeConversion.Global
+            return page.Controls[0] as IPayPalPlugin;
+        }
+
+        public static string GetTitle(this Type pluginType)
+        {
+            return ((AssemblyTitleAttribute)GetAssemblyAttribute(pluginType.Assembly, typeof(AssemblyTitleAttribute))).Title;
+        }
+
+        public static string GetDescription(this Type pluginType)
+        {
+            return ((AssemblyDescriptionAttribute)GetAssemblyAttribute(pluginType.Assembly, typeof(AssemblyDescriptionAttribute))).Description;
+        }
+
+        public static string GetCompany(this Type pluginType)
+        {
+            return ((AssemblyCompanyAttribute)GetAssemblyAttribute(pluginType.Assembly, typeof(AssemblyCompanyAttribute))).Company;
+        }
+
+        private static object GetAssemblyAttribute(Assembly assembly, Type attributeType)
+        {
+            return assembly.GetCustomAttributes(attributeType, true)[0];
+        }
+    }
+}
---
+++ XrmToolBox.Extensibility/Interfaces/IPluginMetadata.cs
@@ -0,0 +1,15 @@
+﻿using System.Drawing;
+
+namespace XrmToolBox.Extensibility.Interfaces
+{
+    public interface IPluginMetadata
+    {
+        string Name { get; }
+        string Description { get; }
+        string BackgroundColor { get; }
+        string PrimaryFontColor { get; }
+        string SecondaryFontColor { get; }
+        string SmallImageBase64 { get; }
+        string BigImageBase64 { get; }
+    }
+}
