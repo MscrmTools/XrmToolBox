036035725d741118670a1770c86a56c86af5ace0
MscrmTools <tanguy92@hotmail.com>
2015-12-20 14:50:15 +0100

    Updated file version configuration

--- Connection Projects/McTools.Xrm.Connection/Properties/AssemblyInfo.cs
+++ Connection Projects/McTools.Xrm.Connection/Properties/AssemblyInfo.cs
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 // Vous pouvez spécifier toutes les valeurs ou indiquer les numéros de build et de révision par défaut
 // en utilisant '*', comme indiqué ci-dessous :
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.2015.10.607")]
-[assembly: AssemblyFileVersion("1.2015.10.607")]
+[assembly: AssemblyVersion("1.2015.12.20")]
+[assembly: AssemblyFileVersion("1.2015.12.20")]
