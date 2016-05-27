9f356fe90f7f6f7a2fd7db1d45d4bbe74d70179f
Tanguy TOUZARD <tanguy92@hotmail.com>
2016-05-27 22:26:36 +0200

    Updated Nuget references (connection and CRM SDK libraries)

--- Plugins/MsCrmTools.SampleTool/Properties/AssemblyInfo.cs
+++ Plugins/MsCrmTools.SampleTool/Properties/AssemblyInfo.cs
@@ -16,5 +16,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 [assembly: Guid("a8234074-9ff2-4a4c-b580-a2ad4507a116")]
-[assembly: AssemblyVersion("1.2016.4.27")]
-[assembly: AssemblyFileVersion("1.2016.4.27")]
+[assembly: AssemblyVersion("1.2016.5.27")]
+[assembly: AssemblyFileVersion("1.2016.5.27")]
