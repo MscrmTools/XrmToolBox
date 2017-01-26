92cf9b068b6b3a3357aecdc7b45d99ac7bd1b04b
Tanguy TOUZARD <tanguy92@hotmail.com>
2017-01-26 13:35:55 +0100

    Updated version number to fix nuget package

--- XrmToolBox/Properties/AssemblyInfo.cs
+++ XrmToolBox/Properties/AssemblyInfo.cs
@@ -36,5 +36,5 @@ using System.Runtime.InteropServices;
 // Vous pouvez spécifier toutes les valeurs ou indiquer les numéros de build et de révision par défaut
 // en utilisant '*', comme indiqué ci-dessous :
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.2017.1.6")]
-[assembly: AssemblyFileVersion("1.2017.1.6")]
+[assembly: AssemblyVersion("1.2017.1.7")]
+[assembly: AssemblyFileVersion("1.2017.1.7")]
