f4937277ae94f05b633400eb40c34ebfc782f26f
Tanguy TOUZARD <tanguy92@hotmail.com>
2017-03-11 20:45:50 +0100

    Fixed NuGet.Core typo

--- XrmToolBox/Program.cs
+++ XrmToolBox/Program.cs
@@ -43,7 +43,7 @@ namespace XrmToolBox
                 CopyUpdatedPlugins();
                 RemovePlugins();
 
-                RedirectAssembly("Nuget.Core");
+                RedirectAssembly("NuGet.Core");
                 RedirectAssembly("McTools.Xrm.Connection");
                 RedirectAssembly("McTools.Xrm.Connection.WinForms");
                 RedirectAssembly("XrmToolBox.Extensibility");
