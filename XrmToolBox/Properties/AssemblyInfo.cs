97b6f27595b6f8f0a2ae5f40637fb20eb3d94890
Tanguy TOUZARD <tanguy92@hotmail.com>
2017-06-10 09:50:00 +0200

    Updated version numbers

--- XrmToolBox/Properties/AssemblyInfo.cs
+++ XrmToolBox/Properties/AssemblyInfo.cs
@@ -36,5 +36,5 @@ using System.Runtime.InteropServices;
 // Vous pouvez spécifier toutes les valeurs ou indiquer les numéros de build et de révision par défaut
 // en utilisant '*', comme indiqué ci-dessous :
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.2017.5.15")]
-[assembly: AssemblyFileVersion("1.2017.5.15")]
+[assembly: AssemblyVersion("1.2017.6.16")]
+[assembly: AssemblyFileVersion("1.2017.6.16")]
