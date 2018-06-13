697f732bd3da8f7872f466a5172a3abc4a0547f8
MscrmTools <tanguy92@hotmail.com>
2018-06-13 09:59:57 +0200

    Updated Nuget references and version numbers

--- Plugins/MsCrmTools.SampleTool/Properties/AssemblyInfo.cs
+++ Plugins/MsCrmTools.SampleTool/Properties/AssemblyInfo.cs
@@ -16,5 +16,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 [assembly: Guid("a8234074-9ff2-4a4c-b580-a2ad4507a116")]
-[assembly: AssemblyVersion("1.2016.8.23")]
-[assembly: AssemblyFileVersion("1.2016.8.23")]
+[assembly: AssemblyVersion("1.2018.6.23")]
+[assembly: AssemblyFileVersion("1.2018.6.23")]
