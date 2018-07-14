6fa15d82e8f738a30ca438a11ba8af20fd8db034
MscrmTools <tanguy92@hotmail.com>
2018-07-14 14:10:45 +0200

    Removed references to TagPage objects that are not used anymore

--- XrmToolBox/Forms/TabConnectionUpdater.Designer.cs
+++ XrmToolBox/Forms/TabConnectionUpdater.Designer.cs
@@ -49,9 +49,10 @@
             this.lvTabs.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
             this.columnHeader1,
             this.columnHeader2});
-            this.lvTabs.Location = new System.Drawing.Point(12, 66);
+            this.lvTabs.Location = new System.Drawing.Point(22, 122);
+            this.lvTabs.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.lvTabs.Name = "lvTabs";
-            this.lvTabs.Size = new System.Drawing.Size(560, 160);
+            this.lvTabs.Size = new System.Drawing.Size(1023, 292);
             this.lvTabs.TabIndex = 0;
             this.lvTabs.UseCompatibleStateImageBehavior = false;
             this.lvTabs.View = System.Windows.Forms.View.Details;
@@ -75,36 +76,41 @@
             this.pnlHeader.Controls.Add(this.lblHeaderDesc);
             this.pnlHeader.Controls.Add(this.lblHeaderTitle);
             this.pnlHeader.Location = new System.Drawing.Point(0, 0);
+            this.pnlHeader.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.pnlHeader.Name = "pnlHeader";
-            this.pnlHeader.Size = new System.Drawing.Size(592, 60);
+            this.pnlHeader.Size = new System.Drawing.Size(1085, 111);
             this.pnlHeader.TabIndex = 1;
             // 
             // lblHeaderDesc
             // 
             this.lblHeaderDesc.AutoSize = true;
-            this.lblHeaderDesc.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblHeaderDesc.Location = new System.Drawing.Point(8, 38);
+            this.lblHeaderDesc.Font = new System.Drawing.Font("Segoe UI Light", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblHeaderDesc.Location = new System.Drawing.Point(15, 54);
+            this.lblHeaderDesc.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
             this.lblHeaderDesc.Name = "lblHeaderDesc";
-            this.lblHeaderDesc.Size = new System.Drawing.Size(351, 13);
+            this.lblHeaderDesc.Size = new System.Drawing.Size(663, 32);
             this.lblHeaderDesc.TabIndex = 1;
-            this.lblHeaderDesc.Text = "Select tabs for which you want to update with the new connection";
+            this.lblHeaderDesc.Tag = "Select tabs for which you want to update with the connection {0}";
+            this.lblHeaderDesc.Text = "Select tabs for which you want to update with the connection {0}";
             // 
             // lblHeaderTitle
             // 
             this.lblHeaderTitle.AutoSize = true;
-            this.lblHeaderTitle.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblHeaderTitle.Location = new System.Drawing.Point(3, 9);
+            this.lblHeaderTitle.Font = new System.Drawing.Font("Segoe UI Light", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblHeaderTitle.Location = new System.Drawing.Point(6, 9);
+            this.lblHeaderTitle.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
             this.lblHeaderTitle.Name = "lblHeaderTitle";
-            this.lblHeaderTitle.Size = new System.Drawing.Size(188, 25);
+            this.lblHeaderTitle.Size = new System.Drawing.Size(312, 45);
             this.lblHeaderTitle.TabIndex = 0;
             this.lblHeaderTitle.Text = "Select tabs to update";
             // 
             // btnOK
             // 
             this.btnOK.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
-            this.btnOK.Location = new System.Drawing.Point(483, 232);
+            this.btnOK.Location = new System.Drawing.Point(886, 428);
+            this.btnOK.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.btnOK.Name = "btnOK";
-            this.btnOK.Size = new System.Drawing.Size(89, 23);
+            this.btnOK.Size = new System.Drawing.Size(163, 42);
             this.btnOK.TabIndex = 3;
             this.btnOK.Text = "&Checked tabs";
             this.btnOK.UseVisualStyleBackColor = true;
@@ -114,9 +120,10 @@
             // 
             this.btnCancel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
             this.btnCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
-            this.btnCancel.Location = new System.Drawing.Point(388, 232);
+            this.btnCancel.Location = new System.Drawing.Point(711, 428);
+            this.btnCancel.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.btnCancel.Name = "btnCancel";
-            this.btnCancel.Size = new System.Drawing.Size(89, 23);
+            this.btnCancel.Size = new System.Drawing.Size(163, 42);
             this.btnCancel.TabIndex = 2;
             this.btnCancel.Text = "&No tabs";
             this.btnCancel.UseVisualStyleBackColor = true;
@@ -125,9 +132,10 @@
             // btnAll
             // 
             this.btnAll.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
-            this.btnAll.Location = new System.Drawing.Point(293, 232);
+            this.btnAll.Location = new System.Drawing.Point(537, 428);
+            this.btnAll.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.btnAll.Name = "btnAll";
-            this.btnAll.Size = new System.Drawing.Size(89, 23);
+            this.btnAll.Size = new System.Drawing.Size(163, 42);
             this.btnAll.TabIndex = 1;
             this.btnAll.Text = "&All tabs";
             this.btnAll.UseVisualStyleBackColor = true;
@@ -136,15 +144,16 @@
             // TabConnectionUpdater
             // 
             this.AcceptButton = this.btnOK;
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 24F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.CancelButton = this.btnCancel;
-            this.ClientSize = new System.Drawing.Size(584, 261);
+            this.ClientSize = new System.Drawing.Size(1071, 482);
             this.Controls.Add(this.btnAll);
             this.Controls.Add(this.btnCancel);
             this.Controls.Add(this.btnOK);
             this.Controls.Add(this.pnlHeader);
             this.Controls.Add(this.lvTabs);
+            this.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.Name = "TabConnectionUpdater";
             this.ShowIcon = false;
             this.Text = "Update connection for opened tabs";
--- XrmToolBox/Forms/TabConnectionUpdater.cs
+++ XrmToolBox/Forms/TabConnectionUpdater.cs
@@ -3,6 +3,7 @@
 // CODEPLEX: http://xrmtoolbox.codeplex.com
 // BLOG: http://mscrmtools.blogspot.com
 
+using McTools.Xrm.Connection;
 using System;
 using System.Collections.Generic;
 using System.Windows.Forms;
@@ -12,28 +13,28 @@ namespace XrmToolBox.Forms
 {
     public partial class TabConnectionUpdater : Form
     {
-        private readonly List<TabPage> tabs;
-
         private readonly List<PluginForm> pluginForms;
 
-        public TabConnectionUpdater(List<TabPage> tabs)
-        {
-            InitializeComponent();
-
-            this.tabs = tabs;
-            SelectedTabs = new List<TabPage>();
-        }
-
-        public TabConnectionUpdater(List<PluginForm> pluginForms)
+        public TabConnectionUpdater(List<PluginForm> pluginForms, ConnectionDetail detail)
         {
             InitializeComponent();
 
             this.pluginForms = pluginForms;
             SelectedPluginForms = new List<PluginForm>();
+
+            lblHeaderDesc.Text = string.Format(lblHeaderDesc.Tag.ToString(), detail.ConnectionName);
         }
 
-        public List<TabPage> SelectedTabs { get; private set; }
-        public List<PluginForm> SelectedPluginForms { get; private set; }
+        public List<PluginForm> SelectedPluginForms { get; }
+
+        private void btnAll_Click(object sender, EventArgs e)
+        {
+            foreach (ListViewItem item in lvTabs.Items)
+            {
+                item.Checked = true;
+            }
+            btnOK.PerformClick();
+        }
 
         private void btnCancel_Click(object sender, EventArgs e)
         {
@@ -45,36 +46,26 @@ namespace XrmToolBox.Forms
         {
             foreach (ListViewItem item in lvTabs.CheckedItems)
             {
-                if (item.Tag is TabPage t)
-                {
-                    SelectedTabs.Add(t);
-                }
-                else
-                {
-                    SelectedPluginForms.Add((PluginForm)item.Tag);
-                }
+                SelectedPluginForms.Add((PluginForm)item.Tag);
             }
 
             DialogResult = DialogResult.OK;
             Close();
         }
 
+        private void EnableButtons()
+        {
+            btnAll.Enabled = lvTabs.Items.Count > 0;
+            btnOK.Enabled = lvTabs.CheckedItems.Count > 0;
+        }
+
+        private void lvTabs_ItemChecked(object sender, ItemCheckedEventArgs e)
+        {
+            EnableButtons();
+        }
+
         private void TabConnectionUpdaterLoad(object sender, EventArgs e)
         {
-            if (tabs != null)
-            {
-                foreach (var tab in tabs)
-                {
-                    string[] nameParts = tab.Text.Split('(');
-
-                    var item = new ListViewItem(nameParts[0]);
-                    item.SubItems.Add(nameParts[1].Replace(")", ""));
-                    item.Tag = tab;
-
-                    lvTabs.Items.Add(item);
-                }
-            }
-
             if (pluginForms != null)
             {
                 foreach (var pluginForm in pluginForms)
@@ -90,26 +81,6 @@ namespace XrmToolBox.Forms
             }
 
             EnableButtons();
-        }
-
-        private void btnAll_Click(object sender, EventArgs e)
-        {
-            foreach (ListViewItem item in lvTabs.Items)
-            {
-                item.Checked = true;
-            }
-            btnOK.PerformClick();
-        }
-
-        private void lvTabs_ItemChecked(object sender, ItemCheckedEventArgs e)
-        {
-            EnableButtons();
-        }
-
-        private void EnableButtons()
-        {
-            btnAll.Enabled = lvTabs.Items.Count > 0;
-            btnOK.Enabled = lvTabs.CheckedItems.Count > 0;
         }
     }
 }
