364a901abd44f38fb539c70ebc1aff4ec44a9e0d
Pascal LEPAISANT <pascal.lepaisant@kleegroup.com>
2018-11-03 14:03:28 +0100

    Handle case when path of xrmtoolbox.exe contains at leat one space

--- XrmToolBox.AutoUpdater/AutoUpdater.cs
+++ XrmToolBox.AutoUpdater/AutoUpdater.cs
@@ -84,7 +84,22 @@ namespace XrmToolBox.AutoUpdater
             args.RemoveAt(0);
             args.RemoveAt(0);
 
-            var xtbPath = args.First();
+            /* handle case where there is at leat one space in the path of the exe */
+            var xtbPath = String.Empty;
+            foreach(var arg in args)
+            {
+                xtbPath = xtbPath + arg + " ";
+                /* the path is complete */
+                if (arg.ToLower().EndsWith("toolbox.exe"))
+                {
+                    break;
+                }
+
+            }
+
+            /* remove extra spaces */
+            xtbPath = xtbPath.Trim();
+
 
             if (!xtbPath.StartsWith("\""))
             {
