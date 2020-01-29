8e73847cb27c256f5f5b814475f7b6815c095e50
MscrmTools <tanguy92@hotmail.com>
2020-01-29 17:12:36 +0100

    Fixed #908 Remove illegal characters from filename when saving settings

--- XrmToolBox.Extensibility/SettingsManager.cs
+++ XrmToolBox.Extensibility/SettingsManager.cs
@@ -1,12 +1,15 @@
-﻿using System;
+﻿using McTools.Xrm.Connection;
+using System;
 using System.IO;
+using System.Linq;
 using System.Xml;
-using McTools.Xrm.Connection;
 
 namespace XrmToolBox.Extensibility
 {
     public class SettingsManager
     {
+        private static readonly char[] IllegalChars = Path.GetInvalidFileNameChars();
+
         /// <summary>
         /// singleton instance of SettingsManager
         /// </summary>
@@ -15,24 +18,13 @@ namespace XrmToolBox.Extensibility
         /// <summary>
         /// Initialize a new instance of class <see cref="SettingsManager"/>
         /// </summary>
-        SettingsManager()
-        {}
+        private SettingsManager()
+        { }
 
         /// <summary>
         /// Gets the singleton instance of SettingsManager
         /// </summary>
-        public static SettingsManager Instance
-        {
-            get
-            {
-                if (_instance == null)
-                {
-                    _instance = new SettingsManager();
-                }
-
-                return _instance;
-            }
-        }
+        public static SettingsManager Instance => _instance ?? (_instance = new SettingsManager());
 
         /// <summary>
         /// Save a settings object to a XML serialized file
@@ -42,19 +34,24 @@ namespace XrmToolBox.Extensibility
         /// <param name="name">Name of the settings file<remarks>Optional parameter that completes the settings file name</remarks></param>
         public void Save(Type pluginType, object settings, string name = null)
         {
+            if (name != null)
+            {
+                name = CleanStringForFileName(name);
+            }
+
             if (!Directory.Exists(Paths.SettingsPath))
             {
                 Directory.CreateDirectory(Paths.SettingsPath);
             }
 
             var filePath = Path.Combine(Paths.SettingsPath,
-                string.Format("{0}{1}{2}.xml", pluginType.Assembly.FullName.Split(',')[0], string.IsNullOrEmpty(name) ? "" : "_", name));
+                $"{pluginType.Assembly.FullName.Split(',')[0]}{(string.IsNullOrEmpty(name) ? "" : "_")}{name}.xml");
 
             XmlSerializerHelper.SerializeToFile(settings, filePath);
 
             // Fix file created before using Assembly name
             filePath = Path.Combine(Paths.SettingsPath,
-               GetSafeFilename(string.Format("{0}{1}{2}.xml", pluginType.Name, string.IsNullOrEmpty(name) ? "" : "_", name)));
+               GetSafeFilename($"{pluginType.Name}{(string.IsNullOrEmpty(name) ? "" : "_")}{name}.xml"));
 
             if (File.Exists(filePath))
             {
@@ -76,41 +73,57 @@ namespace XrmToolBox.Extensibility
         /// </remarks>
         public bool TryLoad<T>(Type pluginType, out T settingsObject, string name = null)
         {
+            if (name != null)
+            {
+                name = CleanStringForFileName(name);
+            }
+
             if (!Directory.Exists(Paths.SettingsPath))
             {
                 Directory.CreateDirectory(Paths.SettingsPath);
             }
 
             var filePath = Path.Combine(Paths.SettingsPath,
-               GetSafeFilename(string.Format("{0}{1}{2}.xml", pluginType.Assembly.FullName.Split(',')[0],
-                    string.IsNullOrEmpty(name) ? "" : "_", name)));
+               GetSafeFilename(
+                   $"{pluginType.Assembly.FullName.Split(',')[0]}{(string.IsNullOrEmpty(name) ? "" : "_")}{name}.xml"));
 
             if (File.Exists(filePath))
             {
                 var document = new XmlDocument();
                 document.Load(filePath);
 
-                settingsObject = (T) XmlSerializerHelper.Deserialize(document.OuterXml, typeof(T));
+                settingsObject = (T)XmlSerializerHelper.Deserialize(document.OuterXml, typeof(T));
                 return true;
             }
 
-            // Check again with a different name to handle settings files 
+            // Check again with a different name to handle settings files
             // created before fixing the name used
             filePath = Path.Combine(Paths.SettingsPath,
-                string.Format("{0}{1}{2}.xml", pluginType.Name, string.IsNullOrEmpty(name) ? "" : "_", name));
+                $"{pluginType.Name}{(string.IsNullOrEmpty(name) ? "" : "_")}{name}.xml");
 
             if (File.Exists(filePath))
             {
                 var document = new XmlDocument();
                 document.Load(filePath);
 
-                settingsObject = (T) XmlSerializerHelper.Deserialize(document.OuterXml, typeof(T));
+                settingsObject = (T)XmlSerializerHelper.Deserialize(document.OuterXml, typeof(T));
                 return true;
             }
 
-
-            settingsObject = default(T);
+            settingsObject = default;
             return false;
+        }
+
+        private string CleanStringForFileName(string text)
+        {
+            var cleanedText = "";
+
+            foreach (var t in text)
+            {
+                cleanedText += IllegalChars.Contains(t) ? '_' : t;
+            }
+
+            return cleanedText;
         }
 
         private string GetSafeFilename(string filename)
