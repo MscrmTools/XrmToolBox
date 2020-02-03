b9390c69703a1dbee912a0c9acd668821a67ade1
MscrmTools <tanguy92@hotmail.com>
2020-02-03 17:30:45 +0100

    Revert "Updated versions"
    This reverts commit 3468dec34b8cd7a390d93c07aa78ea2327f56086.

--- XrmToolBox/Properties/AssemblyInfo.cs
+++ XrmToolBox/Properties/AssemblyInfo.cs
@@ -36,5 +36,5 @@ using System.Runtime.InteropServices;
 // Vous pouvez spécifier toutes les valeurs ou indiquer les numéros de build et de révision par défaut
 // en utilisant '*', comme indiqué ci-dessous :
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.2020.1.36")]
-[assembly: AssemblyFileVersion("1.2020.1.36")]
+[assembly: AssemblyVersion("1.2019.11.35")]
+[assembly: AssemblyFileVersion("1.2019.11.35")]
