abca9b1708e64d243c66394036bd6df878167614
MscrmTools <tanguy92@hotmail.com>
2021-04-07 17:28:59 +0200

    Updated version numbers

--- XrmToolBox/Properties/AssemblyInfo.cs
+++ XrmToolBox/Properties/AssemblyInfo.cs
@@ -36,5 +36,5 @@ using System.Runtime.InteropServices;
 // Vous pouvez spécifier toutes les valeurs ou indiquer les numéros de build et de révision par défaut
 // en utilisant '*', comme indiqué ci-dessous :
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.2021.2.45")]
-[assembly: AssemblyFileVersion("1.2021.2.45")]
+[assembly: AssemblyVersion("1.2021.4.46")]
+[assembly: AssemblyFileVersion("1.2021.4.46")]
