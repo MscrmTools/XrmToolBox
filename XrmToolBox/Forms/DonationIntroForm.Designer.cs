98d9bb501e5f62b11725828179433b4a8a3ba81c
MscrmTools <tanguy92@hotmail.com>
2022-02-28 09:12:09 +0100

    Fixed issue #1115 Order tools for donations

--- XrmToolBox/Forms/DonationIntroForm.Designer.cs
+++ XrmToolBox/Forms/DonationIntroForm.Designer.cs
@@ -57,9 +57,8 @@ namespace XrmToolBox.Forms
             this.pnlHeader.Controls.Add(this.lblHeaderTitle);
             this.pnlHeader.Dock = System.Windows.Forms.DockStyle.Top;
             this.pnlHeader.Location = new System.Drawing.Point(0, 0);
-            this.pnlHeader.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.pnlHeader.Name = "pnlHeader";
-            this.pnlHeader.Size = new System.Drawing.Size(686, 39);
+            this.pnlHeader.Size = new System.Drawing.Size(1029, 60);
             this.pnlHeader.TabIndex = 0;
             // 
             // lblHeaderTitle
@@ -67,9 +66,8 @@ namespace XrmToolBox.Forms
             this.lblHeaderTitle.Dock = System.Windows.Forms.DockStyle.Top;
             this.lblHeaderTitle.Font = new System.Drawing.Font("Segoe UI", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lblHeaderTitle.Location = new System.Drawing.Point(0, 0);
-            this.lblHeaderTitle.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lblHeaderTitle.Name = "lblHeaderTitle";
-            this.lblHeaderTitle.Size = new System.Drawing.Size(686, 38);
+            this.lblHeaderTitle.Size = new System.Drawing.Size(1029, 58);
             this.lblHeaderTitle.TabIndex = 0;
             this.lblHeaderTitle.Text = "Thank you for wanting to donate!";
             // 
@@ -77,19 +75,19 @@ namespace XrmToolBox.Forms
             // 
             this.pnlFooter.Controls.Add(this.btnCancel);
             this.pnlFooter.Dock = System.Windows.Forms.DockStyle.Bottom;
-            this.pnlFooter.Location = new System.Drawing.Point(0, 418);
-            this.pnlFooter.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.pnlFooter.Location = new System.Drawing.Point(0, 643);
             this.pnlFooter.Name = "pnlFooter";
-            this.pnlFooter.Size = new System.Drawing.Size(686, 40);
+            this.pnlFooter.Size = new System.Drawing.Size(1029, 62);
             this.pnlFooter.TabIndex = 1;
             // 
             // btnCancel
             // 
             this.btnCancel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
             this.btnCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
-            this.btnCancel.Location = new System.Drawing.Point(601, 12);
+            this.btnCancel.Location = new System.Drawing.Point(902, 18);
+            this.btnCancel.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.btnCancel.Name = "btnCancel";
-            this.btnCancel.Size = new System.Drawing.Size(75, 23);
+            this.btnCancel.Size = new System.Drawing.Size(112, 35);
             this.btnCancel.TabIndex = 5;
             this.btnCancel.Text = "Cancel";
             this.btnCancel.UseVisualStyleBackColor = true;
@@ -99,11 +97,10 @@ namespace XrmToolBox.Forms
             this.pnlMain.Controls.Add(this.tableLayoutPanel1);
             this.pnlMain.Controls.Add(this.lblDescription);
             this.pnlMain.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.pnlMain.Location = new System.Drawing.Point(0, 39);
-            this.pnlMain.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.pnlMain.Location = new System.Drawing.Point(0, 60);
             this.pnlMain.Name = "pnlMain";
-            this.pnlMain.Padding = new System.Windows.Forms.Padding(7, 6, 7, 6);
-            this.pnlMain.Size = new System.Drawing.Size(686, 379);
+            this.pnlMain.Padding = new System.Windows.Forms.Padding(10, 9, 10, 9);
+            this.pnlMain.Size = new System.Drawing.Size(1029, 583);
             this.pnlMain.TabIndex = 2;
             // 
             // tableLayoutPanel1
@@ -121,26 +118,24 @@ namespace XrmToolBox.Forms
             this.tableLayoutPanel1.Controls.Add(this.btnToolEuros, 1, 3);
             this.tableLayoutPanel1.Controls.Add(this.btnToolPounds, 1, 4);
             this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.tableLayoutPanel1.Location = new System.Drawing.Point(7, 105);
-            this.tableLayoutPanel1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(10, 161);
             this.tableLayoutPanel1.Name = "tableLayoutPanel1";
             this.tableLayoutPanel1.RowCount = 5;
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 32F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 49F));
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 19.94048F));
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 29.76191F));
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
-            this.tableLayoutPanel1.Size = new System.Drawing.Size(672, 268);
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(1009, 413);
             this.tableLayoutPanel1.TabIndex = 4;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Segoe UI", 14F);
-            this.label2.Location = new System.Drawing.Point(338, 0);
-            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Location = new System.Drawing.Point(507, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(223, 25);
+            this.label2.Size = new System.Drawing.Size(328, 38);
             this.label2.TabIndex = 5;
             this.label2.Text = "Donate for a specific tool";
             // 
@@ -150,20 +145,19 @@ namespace XrmToolBox.Forms
             this.cbbTools.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.cbbTools.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.cbbTools.FormattingEnabled = true;
-            this.cbbTools.Location = new System.Drawing.Point(338, 34);
-            this.cbbTools.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.cbbTools.Location = new System.Drawing.Point(507, 52);
             this.cbbTools.Name = "cbbTools";
-            this.cbbTools.Size = new System.Drawing.Size(332, 33);
+            this.cbbTools.Size = new System.Drawing.Size(499, 46);
+            this.cbbTools.Sorted = true;
             this.cbbTools.TabIndex = 1;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Segoe UI", 14F);
-            this.label1.Location = new System.Drawing.Point(2, 0);
-            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Location = new System.Drawing.Point(3, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(207, 25);
+            this.label1.Size = new System.Drawing.Size(305, 38);
             this.label1.TabIndex = 3;
             this.label1.Text = "Donate for XrmToolBox";
             // 
@@ -172,10 +166,9 @@ namespace XrmToolBox.Forms
             this.btnXtbDollars.Dock = System.Windows.Forms.DockStyle.Fill;
             this.btnXtbDollars.Image = global::XrmToolBox.Properties.Resources.money_dollar;
             this.btnXtbDollars.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.btnXtbDollars.Location = new System.Drawing.Point(2, 81);
-            this.btnXtbDollars.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnXtbDollars.Location = new System.Drawing.Point(3, 124);
             this.btnXtbDollars.Name = "btnXtbDollars";
-            this.btnXtbDollars.Size = new System.Drawing.Size(332, 66);
+            this.btnXtbDollars.Size = new System.Drawing.Size(498, 102);
             this.btnXtbDollars.TabIndex = 6;
             this.btnXtbDollars.Text = "In US Dollars";
             this.btnXtbDollars.UseVisualStyleBackColor = true;
@@ -186,10 +179,9 @@ namespace XrmToolBox.Forms
             this.btnXtbEuros.Dock = System.Windows.Forms.DockStyle.Fill;
             this.btnXtbEuros.Image = global::XrmToolBox.Properties.Resources.money_euro;
             this.btnXtbEuros.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.btnXtbEuros.Location = new System.Drawing.Point(2, 151);
-            this.btnXtbEuros.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnXtbEuros.Location = new System.Drawing.Point(3, 232);
             this.btnXtbEuros.Name = "btnXtbEuros";
-            this.btnXtbEuros.Size = new System.Drawing.Size(332, 55);
+            this.btnXtbEuros.Size = new System.Drawing.Size(498, 85);
             this.btnXtbEuros.TabIndex = 7;
             this.btnXtbEuros.Text = "In Euros";
             this.btnXtbEuros.UseVisualStyleBackColor = true;
@@ -200,10 +192,9 @@ namespace XrmToolBox.Forms
             this.btnXtbPounds.Dock = System.Windows.Forms.DockStyle.Fill;
             this.btnXtbPounds.Image = global::XrmToolBox.Properties.Resources.money_pound;
             this.btnXtbPounds.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.btnXtbPounds.Location = new System.Drawing.Point(2, 210);
-            this.btnXtbPounds.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnXtbPounds.Location = new System.Drawing.Point(3, 323);
             this.btnXtbPounds.Name = "btnXtbPounds";
-            this.btnXtbPounds.Size = new System.Drawing.Size(332, 56);
+            this.btnXtbPounds.Size = new System.Drawing.Size(498, 87);
             this.btnXtbPounds.TabIndex = 8;
             this.btnXtbPounds.Text = "In Pounds Sterling";
             this.btnXtbPounds.UseVisualStyleBackColor = true;
@@ -213,10 +204,9 @@ namespace XrmToolBox.Forms
             // 
             this.btnToolDollars.Image = global::XrmToolBox.Properties.Resources.money_dollar;
             this.btnToolDollars.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.btnToolDollars.Location = new System.Drawing.Point(338, 81);
-            this.btnToolDollars.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnToolDollars.Location = new System.Drawing.Point(507, 124);
             this.btnToolDollars.Name = "btnToolDollars";
-            this.btnToolDollars.Size = new System.Drawing.Size(332, 66);
+            this.btnToolDollars.Size = new System.Drawing.Size(498, 102);
             this.btnToolDollars.TabIndex = 9;
             this.btnToolDollars.Text = "In US Dollars";
             this.btnToolDollars.UseVisualStyleBackColor = true;
@@ -227,10 +217,9 @@ namespace XrmToolBox.Forms
             this.btnToolEuros.Dock = System.Windows.Forms.DockStyle.Fill;
             this.btnToolEuros.Image = global::XrmToolBox.Properties.Resources.money_euro;
             this.btnToolEuros.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.btnToolEuros.Location = new System.Drawing.Point(338, 151);
-            this.btnToolEuros.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnToolEuros.Location = new System.Drawing.Point(507, 232);
             this.btnToolEuros.Name = "btnToolEuros";
-            this.btnToolEuros.Size = new System.Drawing.Size(332, 55);
+            this.btnToolEuros.Size = new System.Drawing.Size(499, 85);
             this.btnToolEuros.TabIndex = 10;
             this.btnToolEuros.Text = "In Euros";
             this.btnToolEuros.UseVisualStyleBackColor = true;
@@ -241,10 +230,9 @@ namespace XrmToolBox.Forms
             this.btnToolPounds.Dock = System.Windows.Forms.DockStyle.Fill;
             this.btnToolPounds.Image = global::XrmToolBox.Properties.Resources.money_pound;
             this.btnToolPounds.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.btnToolPounds.Location = new System.Drawing.Point(338, 210);
-            this.btnToolPounds.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnToolPounds.Location = new System.Drawing.Point(507, 323);
             this.btnToolPounds.Name = "btnToolPounds";
-            this.btnToolPounds.Size = new System.Drawing.Size(332, 56);
+            this.btnToolPounds.Size = new System.Drawing.Size(499, 87);
             this.btnToolPounds.TabIndex = 11;
             this.btnToolPounds.Text = "In Pounds Sterling";
             this.btnToolPounds.UseVisualStyleBackColor = true;
@@ -254,23 +242,21 @@ namespace XrmToolBox.Forms
             // 
             this.lblDescription.Dock = System.Windows.Forms.DockStyle.Top;
             this.lblDescription.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblDescription.Location = new System.Drawing.Point(7, 6);
-            this.lblDescription.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblDescription.Location = new System.Drawing.Point(10, 9);
             this.lblDescription.Name = "lblDescription";
-            this.lblDescription.Size = new System.Drawing.Size(672, 99);
+            this.lblDescription.Size = new System.Drawing.Size(1009, 152);
             this.lblDescription.TabIndex = 0;
             this.lblDescription.Text = resources.GetString("lblDescription.Text");
             // 
             // DonationIntroForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
-            this.ClientSize = new System.Drawing.Size(686, 458);
+            this.ClientSize = new System.Drawing.Size(1029, 705);
             this.Controls.Add(this.pnlMain);
             this.Controls.Add(this.pnlFooter);
             this.Controls.Add(this.pnlHeader);
-            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "DonationIntroForm";
             this.ShowIcon = false;
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
