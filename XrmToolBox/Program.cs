f5e92707f1506e71ea7db89b03b1782548829ad2
MscrmTools <tanguy92@hotmail.com>
2023-01-31 20:42:12 +0100

    Added binding redirect for some assemblies to fix things + updated version

--- XrmToolBox/Program.cs
+++ XrmToolBox/Program.cs
@@ -323,6 +323,7 @@ Please start XrmToolBox again to fix this problem",
             RedirectAssembly("WeifenLuo.WinFormsUI.Docking");
             RedirectAssembly("WeifenLuo.WinFormsUI.Docking.ThemeVS2015");
             RedirectAssembly("ScintillaNET");
+            RedirectAssembly("Microsoft.Web.WebView2.Core");
             RedirectAssembly("Microsoft.Web.WebView2.WinForms");
 
             OptimizeConnectionSettings();
