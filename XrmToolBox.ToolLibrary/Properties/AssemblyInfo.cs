9f4f588ac940f9d00d3e9220e49b1ff6cb6ce82f
MscrmTools <tanguy92@hotmail.com>
2025-07-27 14:58:08 +0200

    Updated version number to fix portal release notes page issue

--- XrmToolBox.ToolLibrary/Properties/AssemblyInfo.cs
+++ XrmToolBox.ToolLibrary/Properties/AssemblyInfo.cs
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 // Vous pouvez spécifier toutes les valeurs ou indiquer les numéros de build et de révision par défaut
 // en utilisant '*', comme indiqué ci-dessous :
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.2024.9.69")]
-[assembly: AssemblyFileVersion("1.2024.9.69")]
+[assembly: AssemblyVersion("1.2025.7.71")]
+[assembly: AssemblyFileVersion("1.2025.7.71")]
