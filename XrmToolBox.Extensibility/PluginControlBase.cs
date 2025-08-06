0b3ac49dce9d6fea454bbeaa02e33f6cee6ad513
Jonas Rapp <jonas@jonasr.app>
2025-08-06 21:44:59 +0200

    Added method LogVerbose in PluginControlBase

--- XrmToolBox.Extensibility/PluginControlBase.cs
+++ XrmToolBox.Extensibility/PluginControlBase.cs
@@ -391,6 +391,16 @@ namespace XrmToolBox.Extensibility
         }
 
         /// <summary>
+        /// Writes a verbose message in the log
+        /// </summary>
+        /// <param name="message"></param>
+        /// <param name="args"></param>
+        public void LogVerbose(string message, params object[] args)
+        {
+            logManager.LogVerbose(message, args);
+        }
+
+        /// <summary>
         /// Writes a warning message in the log
         /// </summary>
         /// <param name="message">Message</param>
